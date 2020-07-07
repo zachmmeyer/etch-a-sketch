3594be1ce26607c6cdf11661477f93340f2382d4
zachmmeyer <zachary.michael.meyer@gmail.com>
2020-07-07 15:43:31 -0400

    Comment out everything that is not a table

--- script.js
+++ script.js
@@ -15,24 +15,24 @@ function generateContainers (gridSize,sketchContainer,individualContainer) {
         sketchContainer.appendChild(individualContainer.cloneNode(true));
         //sketchContainer.childNodes[i].setAttribute('data-key', `${i}`);
         sketchContainer.childNodes[i].classList.add(`${i}`)
-        if (sketchContainer.childNodes[i].classList.contains('0')){
-            sketchContainer.childNodes[i].classList.add('top-left-corner');
-        }
-        if (sketchContainer.childNodes[i].classList.contains(gridSize-1)) {
-            sketchContainer.childNodes[i].classList.add('bottom-right-corner');
-        }
-        if (sketchContainer.childNodes[i].classList.contains(gridSize-(Math.sqrt(gridSize)))) {
-           sketchContainer.childNodes[i].classList.add('bottom-left-corner')
-        }
-        if (sketchContainer.childNodes[i].classList.contains(Math.sqrt(gridSize)-1)) {
-            sketchContainer.childNodes[i].classList.add('top-right-corner');
-        }
+        // if (sketchContainer.childNodes[i].classList.contains('0')){
+        //     sketchContainer.childNodes[i].classList.add('top-left-corner');
+        // }
+        // if (sketchContainer.childNodes[i].classList.contains(gridSize-1)) {
+        //     sketchContainer.childNodes[i].classList.add('bottom-right-corner');
+        // }
+        // if (sketchContainer.childNodes[i].classList.contains(gridSize-(Math.sqrt(gridSize)))) {
+        //    sketchContainer.childNodes[i].classList.add('bottom-left-corner')
+        // }
+        // if (sketchContainer.childNodes[i].classList.contains(Math.sqrt(gridSize)-1)) {
+        //     sketchContainer.childNodes[i].classList.add('top-right-corner');
+        // }
     }
     return;
 }
 
 function main () {
-    let gridSize = 400;
+    let gridSize = 36;
     const sketchContainer = document.querySelector('.sketch-container');
     const individualContainer = document.createElement('div');
     individualContainer.classList.add('individual-container');
@@ -45,18 +45,23 @@ main();
 
 /*
 
+Use a table you idiot
+
+-------------------------------------------------
+
 Edge Logic Pseudocode
 
-0  1  2  3  4
-5  6  7  8  9
-10 11 12 13 14
-15 16 17 18 19
-20 21 22 23 24
+0  1  2  3  4  5
+6  7  8  9  10 11
+12 13 14 15 16 17
+18 19 20 21 22 23
+24 25 26 27 28 29
+30 31 32 33 34 35
 
 0 is always top left
 gridSize - 1 is always bottom right
 gridSize - square root of gridSize is bottom left
-0 + square root of gridSize is top right
+square root of gridSize - 1 is top right
 
 -------------------------------------------------
 
