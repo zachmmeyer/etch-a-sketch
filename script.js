9430192b92a5202f625a0e2734de643484a6596e
zachmmeyer <zachary.michael.meyer@gmail.com>
2020-07-07 16:16:33 -0400

    Clean up commented garbage

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ function gridTemplateString (gridSize) {
 function generateContainers (gridSize,sketchContainer,individualContainer) {
     for (let i = 0; i < gridSize; i++) {
         sketchContainer.appendChild(individualContainer.cloneNode(true));
-        //sketchContainer.childNodes[i].setAttribute('data-key', `${i}`);
         sketchContainer.childNodes[i].classList.add(`${i}`);
         if (sketchContainer.childNodes[i].classList.contains('0')){
             sketchContainer.childNodes[i].classList.add('top-left-corner');
@@ -46,30 +45,5 @@ main();
 /*
 
 All borders are now 2px, except the outer walls. Easy enough to do with math.
-
--------------------------------------------------
-
-Use a table you idiot
-
--------------------------------------------------
-
-Edge Logic Pseudocode
-
-0  1  2  3  4  5
-6  7  8  9  10 11
-12 13 14 15 16 17
-18 19 20 21 22 23
-24 25 26 27 28 29
-30 31 32 33 34 35
-
-0 is always top left
-gridSize - 1 is always bottom right
-gridSize - square root of gridSize is bottom left
-square root of gridSize - 1 is top right
-
--------------------------------------------------
-
-Should make grid out of square numbers only, minimum of 16
-Should use an automatically generated slider up to a high amount maybe.
 
 */
