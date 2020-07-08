0624dfa25c7c0c09b36ddf2415bac0dc2d52eaf3
zachmmeyer <zachary.michael.meyer@gmail.com>
2020-07-07 20:52:03 -0400

    Add grid array function

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function gridTemplateString (gridSize) {
-    const gridSizeRt = Math.sqrt(gridSize);
-    let gridString = '';
-    for (let i = 0; i < gridSizeRt; i++) {
-        gridString += 'auto';
-        if (i < (gridSizeRt-1)) {
-            gridString += ' ';
-        }
-    }
-    return gridString;
-}
-
 function generateContainers (gridSize,sketchContainer,individualContainer) {
     for (let i = 0; i < gridSize; i++) {
         sketchContainer.appendChild(individualContainer.cloneNode(true));
@@ -30,6 +18,35 @@ function generateContainers (gridSize,sketchContainer,individualContainer) {
     return;
 }
 
+function gridTemplateString (gridSize) {
+    const gridSizeRt = Math.sqrt(gridSize);
+    let gridString = '';
+    for (let i = 0; i < gridSizeRt; i++) {
+        gridString += 'auto';
+        if (i < (gridSizeRt-1)) {
+            gridString += ' ';
+        }
+    }
+    return gridString;
+}
+
+function gridArrayFill (gridSize) {
+    let gridArray = [];
+    let mdArray = [];
+    const gridSizeRt = Math.sqrt(gridSize);
+    for (let i = 0; i < gridSize; i++) {
+        gridArray.push(i);
+        if (gridArray.length == gridSizeRt) {
+            mdArray.push(gridArray.splice(0,gridSizeRt));
+        }
+    }
+    return mdArray;
+}
+
+function borderHandler (gridArray) {
+    
+}
+
 function main () {
     let gridSize = 36;
     const sketchContainer = document.querySelector('.sketch-container');
@@ -38,11 +55,26 @@ function main () {
     const newGridSize = gridTemplateString(gridSize);
     sketchContainer.setAttribute('style', `grid-template-columns: ${newGridSize}; grid-template-rows: ${newGridSize};`);
     generateContainers(gridSize,sketchContainer,individualContainer);
+    const gridArray = gridArrayFill(gridSize);
+    borderHandler(gridArray);
 }
 
 main();
 
 /*
+
+0  1  2  3  4  5
+6  7  8  9  10 11
+12 13 14 15 16 17
+18 19 20 21 22 23
+24 25 26 27 28 29
+30 31 32 33 34 35
+
+
+
+Slider range 4 - 30, number picked is grid size.
+
+-------------------------------------------------
 
 All borders are now 2px, except the outer walls. Easy enough to do with math.
 
