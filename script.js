a736a86f4267be69c1966e3ebb6cf235af136890
zachmmeyer <zachary.michael.meyer@gmail.com>
2020-07-09 18:52:28 -0400

    Add ESLint AirBNB standards

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function generateContainers(gridSize, sketchContainer, individualContainer) {
-  for (let i = 0; i < gridSize; i++) {
+  for (let i = 0; i < gridSize; i += 1) {
     sketchContainer.appendChild(individualContainer.cloneNode(true));
     sketchContainer.childNodes[i].classList.add(`${i}`);
     if (sketchContainer.childNodes[i].classList.contains('0')) {
@@ -10,7 +10,7 @@ function generateContainers(gridSize, sketchContainer, individualContainer) {
     }
     if (
       sketchContainer.childNodes[i].classList.contains(
-        gridSize - Math.sqrt(gridSize)
+        gridSize - Math.sqrt(gridSize),
       )
     ) {
       sketchContainer.childNodes[i].classList.add('bottom-left-corner');
@@ -21,13 +21,12 @@ function generateContainers(gridSize, sketchContainer, individualContainer) {
       sketchContainer.childNodes[i].classList.add('top-right-corner');
     }
   }
-  return;
 }
 
 function gridTemplateString(gridSize) {
   const gridSizeRt = Math.sqrt(gridSize);
   let gridString = '';
-  for (let i = 0; i < gridSizeRt; i++) {
+  for (let i = 0; i < gridSizeRt; i += 1) {
     gridString += 'auto';
     if (i < gridSizeRt - 1) {
       gridString += ' ';
@@ -40,44 +39,44 @@ function borderHandler(gridSize, sketchContainer) {
   const gridSizeRt = Math.sqrt(gridSize);
   let leftNum = 0;
   let rightNum = 0;
-  for (let i = 0; i < gridSize; i++) {
-    leftNum++;
-    rightNum++;
+  for (let i = 0; i < gridSize; i += 1) {
+    leftNum += 1;
+    rightNum += 1;
     if (
-      leftNum == gridSizeRt &&
-      sketchContainer.childNodes[i + 1] != undefined
+      leftNum === gridSizeRt &&
+      sketchContainer.childNodes[i + 1] !== undefined
     ) {
       sketchContainer.childNodes[i + 1].classList.add('left-side');
       leftNum = 0;
     }
     if (
-      rightNum == gridSizeRt &&
-      sketchContainer.childNodes[i] != gridSize &&
-      sketchContainer.childNodes[i] != gridSizeRt - 1
+      rightNum === gridSizeRt &&
+      sketchContainer.childNodes[i] !== gridSize &&
+      sketchContainer.childNodes[i] !== gridSizeRt - 1
     ) {
       sketchContainer.childNodes[i].classList.add('right-side');
       rightNum = 0;
     }
   }
-  for (let i = 0; i < gridSizeRt; i++) {
-    if (i != 0 && i != gridSizeRt) {
+  for (let i = 0; i < gridSizeRt; i += 1) {
+    if (i !== 0 && i !== gridSizeRt) {
       sketchContainer.childNodes[i].classList.add('top-side');
     }
   }
-  for (let i = gridSize - 1; i > gridSize - 1 - gridSizeRt; i--) {
+  for (let i = gridSize - 1; i > gridSize - 1 - gridSizeRt; i -= 1) {
     sketchContainer.childNodes[i].classList.add('bottom-side');
   }
 }
 
 function main() {
-  let gridSize = 36;
+  const gridSize = 36;
   const sketchContainer = document.querySelector('.sketch-container');
   const individualContainer = document.createElement('div');
   individualContainer.classList.add('individual-container');
   const newGridSize = gridTemplateString(gridSize);
   sketchContainer.setAttribute(
     'style',
-    `grid-template-columns: ${newGridSize}; grid-template-rows: ${newGridSize};`
+    `grid-template-columns: ${newGridSize}; grid-template-rows: ${newGridSize};`,
   );
   generateContainers(gridSize, sketchContainer, individualContainer);
   borderHandler(gridSize, sketchContainer);
